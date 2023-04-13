fd207df120dbaabf4784c432099704c416f9c744
Gabe Tucker <tucker.854@osu.edu>
2023-04-13 04:44:02 -0400

    wip

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 	epochs          = 20
 	epoch           = 0
 	secondsPerEpoch = 0.1
-	epochHistory    = make1DMatrix(simulations, -1)
+	epochHistory    = makeVector(simulations, -1)
 )
 
 var (
@@ -37,7 +37,7 @@ var (
 var (
 	width  = 30
 	height = 10
-	enviro = make2DMatrix(width, height, 0)
+	enviro = makeMatrix(width, height, 0)
 
 	agentX = 0
 	agentY = 0
@@ -52,12 +52,15 @@ var (
 	outputCount = 2
 
 	alpha         = 0.01
-	biasDeviation = 0.01
+	noiseDeviation = 0.01
 )
 
 var (
-	inputToHiddenWeights  = make2DMatrix(inputCount, hiddenCount, 0.5)
-	hiddenToOutputWeights = make2DMatrix(hiddenCount, outputCount, 0.5)
+	inputToHiddenWeights  = makeMatrix(inputCount, hiddenCount, 0.5)
+	hiddenToOutputWeights = makeMatrix(hiddenCount, outputCount, 0.5)
+
+	inputToHiddenNoise = makeNoise(hiddenCount)
+	hiddenToOutputNoise = makeNoise(outputCount)
 )
 
 func main() {
@@ -86,9 +89,9 @@ func main() {
 		// run main functions iteratively
 		for epoch := 0; epoch < epochs; epoch++ {
 
-			outputActivations := think()
+			hiddenActivations, outputActivations := think()
 			move(outputActivations)
-			learn(outputActivations)
+			learn(hiddenActivations, outputActivations)
 
 			if renderMap {
 				updateMap()
@@ -128,21 +131,52 @@ func linearReg(data []float64) (float64, float64, float64) {
 	return predictedStart, predictedEnd, predictedEnd - predictedStart
 }
 
-func think() []float64 {
+func think() ([]float64, []float64) {
 
 	inputActivations := []float64{float64(foodX), float64(foodY)}
-	hiddenActivations := make1DMatrix(hiddenCount, toBeUpdated)
-	outputActivations := make1DMatrix(outputCount, toBeUpdated)
+	hiddenActivations := makeVector(hiddenCount, toBeUpdated)
+	outputActivations := makeVector(outputCount, toBeUpdated)
 
-	inputToHiddenAxonPotentials := dot(getAxonPotentials(inputActivations, inputToHiddenWeights), makeBiases(hiddenCount))
-
+	// forwardpropagate
+	inputToHiddenAxonPotentials := plus(dot(inputActivations, inputToHiddenWeights), inputToHiddenNoise)
 	hiddenActivations = getPostsynapticActivations(inputToHiddenAxonPotentials)
 
-	hiddenToOutputAxonPotentials := dot(getAxonPotentials(hiddenActivations, hiddenToOutputWeights), makeBiases(outputCount))
-
+	hiddenToOutputAxonPotentials := plus(dot(hiddenActivations, hiddenToOutputWeights), hiddenToOutputNoise)
 	outputActivations = getPostsynapticActivations(hiddenToOutputAxonPotentials)
-	return outputActivations
 
+	return hiddenActivations, outputActivations
+
+}
+
+// getAxonPotentials
+func dot(vector []float64, matrix [][]float64) []float64 {
+	N := len(matrix)
+	M := len(matrix[0])
+	axonPotentials := make([]float64, N)
+	for n := 0; n < N; n++ {
+		for m := 0; m < M; m++ {
+			axonPotentials[n] += vector[m] * matrix[m][n]
+		}
+	}
+	return axonPotentials
+}
+
+func getPostsynapticActivations(axonPotentials []float64) []float64 {
+	N := len(axonPotentials)
+	postsynapticActivations := make([]float64, N)
+	for n := 0; n < N; n++ {
+		postsynapticActivations[n] = activationFunction(axonPotentials[n])
+	}
+	return postsynapticActivations
+}
+
+func makeNoise(N int) []float64 {
+	// AKA noisees
+	noise := make([]float64, N)
+	for n := 0; n < N; n++ {
+		noise[n] = norm(noiseDeviation, 0, 1)
+	}
+	return noise
 }
 
 func move(outputActivations []float64) {
@@ -155,7 +189,7 @@ func move(outputActivations []float64) {
 
 }
 
-func learn(outputActivations []float64) {
+func learn(hiddenActivations, outputActivations []float64) {
 
 	// update target
 	target := []float64{toBeUpdated, toBeUpdated}
@@ -175,10 +209,19 @@ func learn(outputActivations []float64) {
 	}
 
 	// backpropagate
-
 	outputError := minus(outputActivations, target)
+	outputDerivative := times(outputActivations, minus(makeVector(outputCount, 1), outputActivations))
+	outputDelta := times(outputError, outputDerivative)
 
-	outputDerivative = outputActivations * minus(make1DMatrix(outputCount, 1), outputActivations)
+	hiddenError := dot(outputDelta, transpose(hiddenToOutputWeights))
+	hiddenDerivative := times(hiddenActivations, minus(makeVector(hiddenCount, 1), hiddenActivations))
+	hiddenDelta := times(hiddenError, hiddenDerivative)
+
+	// update
+	hiddenToOutputWeights = minus2D(hiddenToOutputWeights, times(dot(hiddenActivations, outputDelta), makeVector(hiddenCount, alpha)))
+	// hiddenToOutputNoise = minus(hiddenToOutputNoise, times(hiddenDelta, makeVector(outputCount, alpha)))
+	inputToHiddenWeights = minus2D()
+	// inputToHiddenNoise = 
 
 }
 
@@ -194,27 +237,6 @@ func norm(x, mean, stddev float64) float64 {
 	exponent := -(math.Pow(x-mean, 2.0) / (2.0 * math.Pow(stddev, 2.0)))
 	denominator := stddev * math.Sqrt(2.0*math.Pi)
 	return math.Exp(exponent) / denominator
-}
-
-func getAxonPotentials(presynapticActivations []float64, weights [][]float64) []float64 {
-	N := len(weights)
-	M := len(weights[0])
-	axonPotentials := make([]float64, N)
-	for n := 0; n < N; n++ {
-		for m := 0; m < M; m++ {
-			axonPotentials[n] += presynapticActivations[m] * weights[m][n]
-		}
-	}
-	return axonPotentials
-}
-
-func getPostsynapticActivations(axonPotentials []float64) []float64 {
-	N := len(axonPotentials)
-	postsynapticActivations := make([]float64, N)
-	for n := 0; n < N; n++ {
-		postsynapticActivations[n] = activationFunction(axonPotentials[n])
-	}
-	return postsynapticActivations
 }
 
 func foundFood() bool {
@@ -237,7 +259,7 @@ func updateMap() {
 	}
 }
 
-func make1DMatrix(M int, v float64) []float64 {
+func makeVector(M int, v float64) []float64 {
 	mat := make([]float64, M)
 	for x := 0; x < M; x++ {
 		mat[x] = v
@@ -245,7 +267,7 @@ func make1DMatrix(M int, v float64) []float64 {
 	return mat
 }
 
-func make2DMatrix(M, N int, v float64) [][]float64 {
+func makeMatrix(M, N int, v float64) [][]float64 {
 	mat := make([][]float64, M)
 	for x := 0; x < M; x++ {
 		mat[x] = make([]float64, N)
@@ -256,15 +278,22 @@ func make2DMatrix(M, N int, v float64) [][]float64 {
 	return mat
 }
 
-func makeBiases(N int) []float64 {
-	biases := make([]float64, N)
-	for n := 0; n < N; n++ {
-		biases[n] = norm(biasDeviation, 0, 1)
-	}
-	return biases
+func transpose(mat [][]float64) [][]float64 {
+    rows := len(mat)
+    cols := len(mat[0])
+    transposed := make([][]float64, cols)
+    for i := range transposed {
+        transposed[i] = make([]float64, rows)
+    }
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            transposed[j][i] = mat[i][j]
+        }
+    }
+    return transposed
 }
 
-func dot(v1, v2 []float64) []float64 {
+func plus(v1, v2 []float64) []float64 {
 	N := len(v1)
 	v := make([]float64, N)
 	for n := 0; n < N; n++ {
@@ -280,6 +309,20 @@ func minus(v1, v2 []float64) []float64 {
 		v[n] = v1[n] - v2[n]
 	}
 	return v
+}
+
+func minus2D(m1, m2 [][]float64) [][]float64 {
+    numRows := len(m1)
+    numCols := len(m1[0])
+    result := make([][]float64, numRows)
+    for i := 0; i < numRows; i++ {
+        row := make([]float64, numCols)
+        for j := 0; j < numCols; j++ {
+            row[j] = m1[i][j] - m2[i][j]
+        }
+        result[i] = row
+    }
+    return result
 }
 
 func times(v1, v2 []float64) []float64 {
